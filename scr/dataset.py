925537d85283910a6e3638ad92286fa8df7a6a5f
LassilaJoni <joni@lassila.live>
2024-01-25 21:23:50 +0200

    Added commenting to dataset.py

--- scr/dataset.py
+++ scr/dataset.py
@@ -15,7 +15,17 @@ from torchvision import transforms
 import albumentations as A
 
 class MammoDataset(Dataset):
+    """
+    Mammodataset class it's used for loading and
+    transforming mammography images and masks
+    """
     def __init__(self, path, dataset, split, augmentations=False):
+        """ used to initialize class with required parameters
+        :param path: Path to the dataset
+        :param dataset: Name of the dataset folder
+        :param split: Type of datasetsplit what is used such as 'train', 'valid' or 'test'
+
+        """
         self.path = path
         self.split = split
         self.dataset = dataset
@@ -28,14 +38,26 @@ class MammoDataset(Dataset):
         self.contours = natsorted(glob.glob(os.path.join(self.path, self.dataset, self.split, 'dense_mask/*')))
         
     def __len__(self):
+        """
+        :returns: number of items in the dataset
+        """
         return len(self.images)
     
     def __getitem__(self, index):
+        """ A function to retrieve single image and corresponding masks
+        :param index: Index of the data
+        """
 
+        #Converts images into suitable format. First convert to PIL image. Then resize to 256x256. Lastly converts PIL image to tensor and scales the values accordingly.
+        # https://pytorch.org/vision/stable/generated/torchvision.transforms.Compose.html
         self.to_tensor = transforms.Compose([transforms.ToPILImage(),
                                              transforms.Resize((256, 256)),
                                              transforms.ToTensor(),])
 
+        # Uses Albumentations library that is a computer vision tool that can be used to create different variations of the same picture.
+        # Slightly modifes the images to increase the size of training data.
+        # Example: A.ShiftScaleRotate(p = 0.5) randomly apploes translate,scale and rotate to the input. 0.5 is the probability of applying the transform.
+        #https://albumentations.ai/
         self.aug_pipeline = A.Compose([A.ShiftScaleRotate(p = 0.5),
                                        A.HorizontalFlip(0.5),
                                        A.VerticalFlip(0.5),
@@ -44,15 +66,19 @@ class MammoDataset(Dataset):
                                        A.GaussianBlur(p=0.5),
                                        A.ElasticTransform(),],p=0.8)
 
-
+        # Read image
         self.image = cv2.imread(self.images[index], 1)
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
 
+        # Read breast mask and dense mask
         self.mask = cv2.imread(self.masks[index], 0)
         self.contour = cv2.imread(self.contours[index], 0)
 
+        # if augmentation is true (defaulted to false) applies the modification of images to increate the size of training data (self.aug_pipeline)
         if self.augmentations:
+            # Seed for generating random numbers
             torch.manual_seed(1990)
+            # List containing dense and breast masks
             masks = [self.mask, self.contour]
             #if self.augmentation_type == 'pixel':
             #    self.sample = self.pixel_augmentations(image = self.image, masks = masks)
@@ -61,14 +87,23 @@ class MammoDataset(Dataset):
             #elif self.augmentation_type == 'pixel_spatial':
             #    self.sample = self.pixel_spatial_augmentations(image=self.image, masks=masks)
             #elif self.augmentation_type == 'aug_pipeline':
-            self.sample = self.aug_pipeline(image=self.image, masks=masks)
 
+            # Store augmented image
+            self.sample = self.aug_pipeline(image=self.image, masks=masks)
+            # Return augmented image if augmentation
             return self.to_tensor(self.sample['image']), self.to_tensor(self.sample['masks'][0]), self.to_tensor(self.sample['masks'][1])
         else:
+            # Return non augmented if no augmentation
             return self.to_tensor(self.image), self.to_tensor(self.mask), self.to_tensor(self.contour)
 
 class MammoEvaluation(Dataset):
     def __init__(self, path, dataset, split):
+        """ used to initialize class with required parameters
+        :param path: Path to the dataset
+        :param dataset: Name of the dataset folder
+        :param split: Type of datasetsplit what is used such as 'train', 'valid' or 'test'
+
+        """
         self.path = path
         self.split = split
         self.dataset = dataset
@@ -80,9 +115,15 @@ class MammoEvaluation(Dataset):
 
 
     def __len__(self):
+        """
+        :returns: number of items in the dataset
+        """
         return len(self.images)
 
     def __getitem__(self, index):
+        """ A function to retrieve the image and corresponding breast and dense mask
+        :param index: Index of the data
+        """
         self.to_tensor = transforms.Compose([transforms.ToPILImage(),
                                              transforms.Resize((256, 256)),
                                              transforms.ToTensor(), ])
