0b02d9c64aaa3f387f8ec9e6631e936177bd8d4e
rocksanen <rocksanen@hotmail.com>
2024-02-01 21:14:53 +0200

    FTL++

--- scr/segmentation_models_multi_tasking/utils/losses.py
+++ scr/segmentation_models_multi_tasking/utils/losses.py
@@ -3,6 +3,8 @@ import torch
 from . import base
 from . import functional as F
 from  .base import Activation
+from torch.autograd import Variable
+from itertools import filterfalse
 
 
 class JaccardLoss(base.Loss):
@@ -104,7 +106,7 @@ class TverskyLoss(base.Loss):
 
 class FocalTverskyLoss(base.Loss):
 
-    def __init__(self, alpha=0.3,  eps=1., beta=0.7, gamma=0.75,  activation=None, ignore_channels=None, **kwargs):
+    def __init__(self, alpha=0.7,  eps=1., beta=0.3, gamma=0.75,  activation=None, ignore_channels=None, **kwargs):
         super().__init__(**kwargs)
         self.eps = eps
         self.alpha = alpha
@@ -126,6 +128,46 @@ class FocalTverskyLoss(base.Loss):
 
         return torch.pow(tversky_loss, self.gamma)
 
+class FocalTverskyLossPlusPlus(base.Loss):  # under construction
+
+    def __init__(self, alpha=0.3, eps=1e-5, beta=0.7, gamma=0.75, activation=None, ignore_channels=None):
+        super().__init__()
+        self.eps = eps
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma_ftl = gamma  # Renamed to avoid confusion with gamma in DSC++
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        
+        C = y_pr.size(1)  # Number of classes
+        loss = 0.0
+
+        for c in range(C):
+            if self.ignore_channels is not None and c in self.ignore_channels:
+                continue
+
+            y_true_c = y_gt[:, c, ...]
+            y_pred_c = y_pr[:, c, ...]
+
+            tp = torch.sum(y_pred_c * y_true_c, dim=[0, 1, 2])
+            fp = torch.sum(y_pred_c * (1 - y_true_c), dim=[0, 1, 2])
+            fn = torch.sum((1 - y_pred_c) * y_true_c, dim=[0, 1, 2])
+
+            # Modifying the false positives and negatives according to DSC++
+            fp = fp * ((1 - y_true_c) ** self.gamma_ftl)
+            fn = fn * ((1 - y_pred_c) ** self.gamma_ftl)
+
+            numerator = (1 + self.beta ** 2) * tp + self.eps
+            denominator = (1 + self.beta ** 2) * tp + self.beta ** 2 * fn + fp + self.eps
+
+            class_loss = 1 - numerator / denominator
+            loss += torch.pow(class_loss, self.gamma_ftl)  # Applying the focal component
+
+        final_loss = loss.mean()  # Reducing the loss to a scalar
+        return final_loss
 
 class L1Loss(nn.L1Loss, base.Loss):
     pass
@@ -161,3 +203,150 @@ class BCEWithLogitsLoss(nn.BCEWithLogitsLoss, base.Loss):
             threshold=None,
             ignore_channels=self.ignore_channels,
         )
+
+#PyTorch
+ALPHA = 0.5 # < 0.5 penalises FP more, > 0.5 penalises FN more
+BETA = 0.5
+CE_RATIO = 0.5 #weighted contribution of modified CE loss compared to Dice loss
+e = torch.finfo(torch.float32).eps
+
+class ComboLoss(base.Loss):
+    def __init__(self, weight=None, size_average=True):
+        super(ComboLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1, alpha=ALPHA, beta=BETA):
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #True Positives, False Positives & False Negatives
+        intersection = (inputs * targets).sum()    
+        dice = (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+        
+        inputs = torch.clamp(inputs, e, 1.0 - e)       
+        out = - (ALPHA * ((targets * torch.log(inputs)) + ((1 - ALPHA) * (1.0 - targets) * torch.log(1.0 - inputs))))
+        weighted_ce = out.mean(-1)
+        combo = (CE_RATIO * weighted_ce) + ((1 - CE_RATIO) * dice)
+        
+        return combo
+    
+#PyTorch
+class LovaszSoftmaxLoss(base.Loss):
+    def __init__(self, classes='present', per_image=False, ignore=None):
+        super(LovaszSoftmaxLoss, self).__init__()
+        self.classes = classes
+        self.per_image = per_image
+        self.ignore = ignore
+
+    def forward(self, inputs, targets):
+        probas = F.softmax(inputs, dim=1) # B*C*H*W -> from logits to probabilities
+        return lovasz_softmax(probas, targets, self.classes, self.per_image, self.ignore)
+
+def lovasz_softmax(probas, labels, classes='present', per_image=False, ignore=None):
+    """
+    Multi-class Lovasz-Softmax loss
+      probas: [B, C, H, W] Variable, class probabilities at each prediction (between 0 and 1).
+              Interpreted as binary (sigmoid) output with outputs of size [B, H, W].
+      labels: [B, H, W] Tensor, ground truth labels (between 0 and C - 1)
+      classes: 'all' for all, 'present' for classes present in labels, or a list of classes to average.
+      per_image: compute the loss per image instead of per batch
+      ignore: void class labels
+    """
+    if per_image:
+        loss = mean(lovasz_softmax_flat(*flatten_probas(prob.unsqueeze(0), lab.unsqueeze(0), ignore), classes=classes)
+                          for prob, lab in zip(probas, labels))
+    else:
+        loss = lovasz_softmax_flat(*flatten_probas(probas, labels, ignore), classes=classes)
+    return loss
+
+
+def lovasz_softmax_flat(probas, labels, classes='present'):
+    """
+    Multi-class Lovasz-Softmax loss
+      probas: [P, C] Variable, class probabilities at each prediction (between 0 and 1)
+      labels: [P] Tensor, ground truth labels (between 0 and C - 1)
+      classes: 'all' for all, 'present' for classes present in labels, or a list of classes to average.
+    """
+    if probas.numel() == 0:
+        # only void pixels, the gradients should be 0
+        return probas * 0.
+    C = probas.size(1)
+    losses = []
+    class_to_sum = list(range(C)) if classes in ['all', 'present'] else classes
+    for c in class_to_sum:
+        fg = (labels == c).float() # foreground for class c
+        if classes == 'present' and fg.sum() == 0:
+            continue
+        class_pred = probas[:, c]
+        errors = (Variable(fg) - class_pred).abs()
+        errors_sorted, perm = torch.sort(errors, 0, descending=True)
+        perm = perm.data
+        fg_sorted = fg[perm]
+        losses.append(torch.dot(errors_sorted, Variable(lovasz_grad(fg_sorted))))
+    return mean(losses)
+
+
+def flatten_probas(probas, labels, ignore=None):
+    """
+    Flattens predictions in the batch
+    """
+    if probas.dim() == 3:
+        # assumes output of a sigmoid layer
+        B, H, W = probas.size()
+        probas = probas.view(B, 1, H, W)
+    B, C, H, W = probas.size()
+    probas = probas.permute(0, 2, 3, 1).contiguous().view(-1, C)  # B * H * W, C = P, C
+    labels = labels.view(-1)
+    if ignore is None:
+        return probas, labels
+    valid = (labels != ignore)
+    vprobas = probas[valid.nonzero().squeeze()]
+    vlabels = labels[valid]
+    return vprobas, vlabels
+
+def xloss(logits, labels, ignore=None):
+    """
+    Cross entropy loss
+    """
+    return F.cross_entropy(logits, Variable(labels), ignore_index=255)
+
+# --------------------------- HELPER FUNCTIONS ---------------------------
+def isnan(x):
+    return x != x
+    
+    
+def mean(l, ignore_nan=False, empty=0):
+    """
+    nanmean compatible with generators.
+    """
+    l = iter(l)
+    if ignore_nan:
+        l = filterfalse(isnan, l)
+    try:
+        n = 1
+        acc = next(l)
+    except StopIteration:
+        if empty == 'raise':
+            raise ValueError('Empty mean')
+        return empty
+    for n, v in enumerate(l, 2):
+        acc += v
+    if n == 1:
+        return acc
+    return acc / n
+
+
+def lovasz_grad(gt_sorted):
+    """
+    Computes gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    p = len(gt_sorted)
+    gts = gt_sorted.sum()
+    intersection = gts - gt_sorted.float().cumsum(0)
+    union = gts + (1 - gt_sorted).float().cumsum(0)
+    jaccard = 1. - intersection / union
+    if p > 1: # cover 1-pixel case
+        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+    return jaccard
