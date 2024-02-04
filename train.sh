57816564ac1d19fd2539af46b7ece51c80b2cdd8
rocksanen <rocksanen@hotmail.com>
2024-02-04 02:04:13 +0200

    FTL++,ComboLoss and LovazSoftMax

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-python scr/train.py --logs_file_path test_output/logs/test1.txt --model_save_path test_output/models/test1.pth
+python scr/train.py #--logs_file_path test_output/logs/test1.txt --model_save_path test_output/models/test1.pth
 
 #example evaluate command
 #python scr/evaluate.py --data_path ${DIR}/breast-density-prediction/ --dataset test --split test --test_batch_size 1 --num_epochs 10 --num_workers 0 --loss_fucntion LOSS --model_save_path test_output/models/test1.pth --results_path test_output/evaluation/dataset_name.txt
